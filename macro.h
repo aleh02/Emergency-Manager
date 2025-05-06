d8b2eed515c8af02f6fd251020ef27f7f9f6d43d
aleh02 <81700194+aleh02@users.noreply.github.com>
2025-05-06 17:07:11 +0200

    Create macro.h

---
+++ macro.h
@@ -0,0 +1,19 @@
+//syscall error=-1
+#define SCALL_ERROR -1
+#define SCALL(r, c, e) do { if((r = c) == SCALL_ERROR) { perror(e); exit(EXIT_FAILURE); } } while(0)
+//read per return: 0 se EOF, -1 error, per f che return int/ssize_t/read()/...
+#define SCALLREAD(r, loop_cond_op, read_loop_op,e) do {while((r=loop_cond_op)>0) {read_loop_op;} if(r==SCALL_ERROR) {perror(e); exit(EXIT_FAILURE);}} while(0)
+
+//SCALL che non hanno bisogno di var di ritorno
+#define SCALL_VOID(c, e) do { if((c) == SCALL_ERROR) {perror(e); exit(EXIT_FAILURE); } } while(0)
+//SCALL per pointer
+#define SCALL_PTR(r, c, e) do { if((r = c) == (void *) -1) { perror(e); exit(EXIT_FAILURE); } } while(0)
+
+//SNCALL=system null call (error: return NULL), per funzioni che return puntatori
+#define SNCALL(r, c, e) do {if((r = c) == NULL) {perror(e); exit(EXIT_FAILURE);}} while(0)
+//gestione loop lettura
+#define SNCALLREAD(r, loop_cond_op, read_loop_op,e) do {while((r=loop_cond_op)>0) {read_loop_op;} if(errno==SCALL_ERROR) {perror(e); exit(EXIT_FAILURE);}} while(0)
+//opendir e readdir return NULL if error, closedir e stat -1
+
+//per le fork, se pid=0 esegue child
+#define PARENT_OR_CHILD(pid,f_parent,f_child) do { if(pid == 0) { f_child; } else { f_parent; } } while(0)
