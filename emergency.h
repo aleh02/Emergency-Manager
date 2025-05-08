44c747084d6cee298bffb281242001be7030ec8e
Alessandro Han <81700194+aleh02@users.noreply.github.com>
2025-05-08 11:35:35 +0200

    Update emergency.h

--- emergency.h
+++ emergency.h
@@ -1,3 +1,6 @@
+#ifndef EMERGENCY_H
+#define EMERGENCY_H
+
 #include "rescuers.h"
 
 typedef struct {
@@ -12,3 +15,11 @@ typedef struct {
     rescuer_request_t* rescuers;
     int rescuers_req_number;
 } emergency_type_t;
+
+char* trim(char* str);
+
+rescuer_type_t * find_rescuer(char* name, rescuer_type_t* known_types, int known_count);
+
+int parse_emergency_types(char* filename, emergency_type_t ** types_out, int * count_out, rescuer_type_t * known_types, int known_count);
+
+#endif
