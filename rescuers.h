a0faac51bc265c37919b2726c2fea395c5edde14
Alessandro Han <81700194+aleh02@users.noreply.github.com>
2025-05-08 11:33:06 +0200

    Update rescuers.h

--- rescuers.h
+++ rescuers.h
@@ -1,3 +1,6 @@
+#ifndef PARSE_RESCUERS_H
+#define PARSE_RESCUERS_H
+
 typedef enum {
     IDLE, EN_ROUTE_TO_SCENE, ON_SCENE, RETURNING_TO_BASE
 } rescuer_status_t;
@@ -15,3 +18,9 @@ typedef struct {
     rescuer_type_t* rescuer;
     rescuer_status_t status;
 } rescuer_digital_twin_t;
+
+int parse_rescuers(const char* filename, rescuer_type_t** types_out, int* type_count_out, rescuer_digital_twin_t** twins_out, int* twin_count_out);
+
+void remove_brackets(char* str);
+
+#endif
